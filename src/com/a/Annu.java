22ef0aa3d07066b1f62086ef475e559756656f35
nelsonimmco <nelson.b@immcossi.com>
2017-01-24 11:21:20 +0530

    comit

--- src/com/a/Annu.java
+++ src/com/a/Annu.java
@@ -4,12 +4,10 @@ public class Annu {
 
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
 		System.out.println("Hello AnnuPS");
-=======
+
 
 		System.out.println("Hello World!!");
->>>>>>> d8523526201ff6cc4f01e7cc52f3ab1ec9337dcc
 	}
 
 }
