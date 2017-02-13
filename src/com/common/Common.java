2567f20828808ad58bb8128bdeb0200718a3dcba
nelsonimmco <nelson.b@immcossi.com>
2017-02-13 10:06:53 +0530

    again merge

--- src/com/common/Common.java
+++ src/com/common/Common.java
@@ -1,31 +1,18 @@
 package com.common;
 
 public class Common {
-<<<<<<< HEAD
-	public static void main(String[] args){
-		for(int i=0;i<=10;i++){
-		System.out.println("NEW COUNT"+i);
-		}
-=======
-	
-	
-	
-	public static void main(String[] args) {
-		
-		System.out.println("Nivin");
-<<<<<<< HEAD
->>>>>>> c2ad9cf488802263cc662cf5a61c16a89ba72f26
-	}
-=======
-	
->>>>>>> 6e345d442522ad92d78baf73cf82ddc355ca77bd
 
-	
-		for(int count=0;count<5;count++)
-		{
-			System.out.println("Count: "+count);
+	public static void main(String[] args) {
+
+		System.out.println("Nivin");
+
+		for (int i = 0; i <= 10; i++) {
+			System.out.println("NEW COUNT" + i);
+			for (int count = 0; count < 5; count++) {
+				System.out.println("Count: " + count);
+			}
+
+			System.out.println("Done by Nelson");
 		}
-		
-		System.out.println("Done by Nelson");
 	}
 }
