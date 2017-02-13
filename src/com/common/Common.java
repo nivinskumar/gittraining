7d8e363d8cbc15251b8ca72ba44bc6667b11e423
nelsonimmco <nelson.b@immcossi.com>
2017-02-13 09:41:37 +0530

    Nelson commit on common

--- src/com/common/Common.java
+++ src/com/common/Common.java
@@ -2,4 +2,12 @@ package com.common;
 
 public class Common {
 
+	public static void main(String[] args) {
+		for(int count=0;count<5;count++)
+		{
+			System.out.println("Count: "+count);
+		}
+		
+		System.out.println("Done by Nelson");
+	}
 }
